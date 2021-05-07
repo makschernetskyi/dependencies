3c75d4e446bb250585f41fdfe30b35aee5bfe9a7
makschernetskyi <konomerubokkusu@gmail.com>
2021-05-07 20:29:44 +0200

    button to request sort

---
+++ web_client/src/SortButton/SortButton.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { NavLink } from 'react-router-dom'
+//import { sendSortingReqestActionCreator } from '../Redux/selectListReducer'
+
+export const SortButton:React.FC = () => {
+
+	return (
+		<NavLink to="/sorted">
+			<button className="SortButton">sort!</button>
+		</NavLink>
+
+	)
+}
