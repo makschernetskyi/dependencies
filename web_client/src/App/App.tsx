2cd9c23c90b202544c271f9e797c3b19c337df37
makschernetskyi <konomerubokkusu@gmail.com>
2021-05-07 20:27:27 +0200

    types changed

--- web_client/src/App/App.tsx
+++ web_client/src/App/App.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 import { AddSelectFieldButton } from '../AddSelectFieldButton';
-import { SelectList } from '../SelectList'
+import { SelectList } from '../SelectList';
+import { SortButton } from '../SortButton'
+import { SortedDependencies } from '../SortedDependencies/'
+
 
 interface SelectList{
 	options: Array<any>;
 	amountOfSelects: number;
 	selectValues: Array<number>;
+	ordered: Array<number>;
 }
 
 interface State{
@@ -19,6 +25,20 @@ interface Props{
 
 export const App: React.FC<Props> = ({state, dispatch}) =>
 	<div className="App">
-		<SelectList state = {state.selectList} dispatch = {dispatch}/>
-		<AddSelectFieldButton dispatch={dispatch}/>
+	<Router>
+		<Switch>
+
+			<Route exact path='/'>
+				<SelectList state = {state.selectList} dispatch = {dispatch}/>
+				<AddSelectFieldButton dispatch={dispatch}/>
+				<SortButton/>
+			</Route>
+			<Route exact path='/sorted'>
+				<SortedDependencies state = {state.selectList} dispatch = {dispatch}/>
+			</Route>
+
+		</Switch>
+
+
+	</Router>
 	</div>
